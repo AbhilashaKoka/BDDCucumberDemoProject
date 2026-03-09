370db89f367ee85473e6f7974d194ea66d57c060
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2026-03-09 11:31:52 +0530

    locator_01

--- src/test/java/seleniumUITest/locatorUtilites/XPATHSelector.java
+++ src/test/java/seleniumUITest/locatorUtilites/XPATHSelector.java
@@ -1,8 +1,10 @@
-package seleniumUITest.locatorstrategies;
-
+package seleniumUITest.locatorUtilites;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class XPATHSelectorUtility {
+public class XPATHSelector {
 
     // Static utility method to get XPath locators
     public static By byXPath(String xpath) {
@@ -40,5 +42,30 @@ public class XPATHSelectorUtility {
 
     public static By locateByPrecedingSibling(String siblingTag) {
         return byXPath("//preceding-sibling::" + siblingTag);
+    }
+
+    public static void main(String[] args) {
+        // Setup WebDriver
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            // Navigate to a test page
+            driver.get("https://example.com");
+
+            // Using generalized methods to find elements
+            WebElement elementById = driver.findElement(CSSSelector.locateById("submit-button"));
+            WebElement elementByClassName = driver.findElement(CSSSelector.locateByClassName("form-control"));
+            WebElement elementByAttribute = driver.findElement(CSSSelector.locateByAttributeMatching(null,"data-testid", "username","ExactMatch"));
+            WebElement elementByParentChild = driver.findElement(CSSSelector.locateByParentChild("div", "span"));
+            WebElement elementBySibling = driver.findElement(CSSSelector.locateBySiblingPreceding("label", "input"));
+
+            // Perform actions
+            elementById.click();
+            System.out.println("Element interaction completed.");
+
+        } finally {
+            // Close the browser
+            driver.quit();
+        }
     }
 }
